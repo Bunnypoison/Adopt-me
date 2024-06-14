cd1ceeb6e3d866a8b754693170495be617d41321
Alicia <aliciayu22@gmail.com>
2024-06-14 16:13:41 -0600

    add fetch code in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-
+const displayDogs = () => {
+  return fetch("http://localhost:3000/dogs") 
+  .then((resp) => {
+    if (resp.ok) {
+      return resp.json()
+    }})
+  .then((dogs) => dogs.forEach((dog) => {
+    const img = document.createElement("img")
+    img.src = dog.image
+    img.alt = dog.name
+    img.addEventListener("click", () => {handleClick(dog)})
+    document.querySelector('#SOMETHING IN HTML LINE 31').append(img)
+  }))
+}
